eefcd8913dbba1d046fde4a0072e5874d5808434
zkhan93 <zkhan1093@gmail.com>
2015-07-21 17:34:06 +0530

    home url fix

--- wsgi/birthday/birthday/urls.py
+++ wsgi/birthday/birthday/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'myproject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+	url(r'^',include('home.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+)+ static(settings.IMAGE_URL, document_root=settings.IMAGE_ROOT)
