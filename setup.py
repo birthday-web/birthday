0d9baf42b85a3d8447451a61d14fff7b56d4a1e9
zkhan93 <zkhan1093@gmail.com>
2015-07-21 17:18:05 +0530

    rename attempt 2

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
     # GETTING-STARTED: set your app description:
     description='OpenShift App',
     # GETTING-STARTED: set author name (your name):
-    author='Your Name',
+    author='Zeeshan Khan',
     # GETTING-STARTED: set author email (your email):
-    author_email='example@example.com',
+    author_email='zkhan1093@gmail.com',
     # GETTING-STARTED: set author url (your url):
     url='http://www.python.org/sigs/distutils-sig/',
     # GETTING-STARTED: define required django version:
