694583a81fcb993c4fa0613c1bc2b1fda76dab4f
zkhan93 <zkhan1093@gmail.com>
2015-07-31 11:46:31 +0530

    auto commit 2015-07-31_11-46-31

--- wsgi/birthday/birthday/settings.py
+++ wsgi/birthday/birthday/settings.py
@@ -96,7 +96,7 @@ else:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(DB_BASE_DIR,'..', 'db.sqlite3'),
+        'NAME': os.path.join(DB_BASE_DIR, 'db.sqlite3'),
     }
 }
 print '-----',(DATABASES['default'])['NAME'],'-----'
