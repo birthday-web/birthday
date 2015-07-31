ba25c5a7ea2b50b478a4d5140231f501cf253641
zkhan93 <zkhan1093@gmail.com>
2015-07-31 20:25:05 +0530

    auto commit 2015-07-31_20-25-05

--- wsgi/birthday/home/urls.py
+++ wsgi/birthday/home/urls.py
@@ -6,7 +6,7 @@ handler404 = views.index
 
 urlpatterns = [ 
 	url(r'^$', views.index),
-	url(r'^posts/(?P<username>\w{1,50})/$', views.listPosts),
-	url(r'^posts/(?P<username>\w{1,50})/delcmt/(?P<comment_id>\d){1,10}/$', views.delComment),
-	url(r'^posts/(?P<username>\w{1,50})/delpost/(?P<post_id>\d{1,10})/$', views.delPost),
+	url(r'^posts/(?P<username>[-\w.]+)/$', views.listPosts),
+	url(r'^posts/(?P<username>[-\w.]+)/delcmt/(?P<comment_id>\d){1,10}/$', views.delComment),
+	url(r'^posts/(?P<username>[-\w.]+)/delpost/(?P<post_id>\d{1,10})/$', views.delPost),
 ]
